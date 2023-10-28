e93f5189c062a9a24e3b6f998edf2c889d85a2fd
panyu1512 <kikeferreragius1997@gmail.com>
2023-10-28 13:16:18 +0200

    setting up environment variables

--- naixensa_backend/settings.py
+++ naixensa_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import dj_database_url
+import os
 
 from pathlib import Path
 
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*_%%e9(g!m=-)5eee6mcqd2nc5ior0cpd(=um%e9lw7yc96k#2'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
@@ -80,8 +81,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+database_url = os.environ.get('DATABASE_URL')
 
-DATABASES["default"] = dj_database_url.parse("postgres://admin:PZI4GlXuA5gzSlJjPOQJpvoRlEZgIlF2@dpg-ckuek36b0mos7396lq7g-a.frankfurt-postgres.render.com/naixensadb")
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
