aed66120b602565c0ec989f957284f06737288bb
panyu1512 <kikeferreragius1997@gmail.com>
2023-10-30 17:08:37 +0100

    update user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -3,15 +3,22 @@ from rest_framework import serializers
 from rest_framework_simplejwt.tokens import Token
 from .models import User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["email", "name", "last_name", "id", "is_staff"]
+
 class RegisterUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'name', 'last_name', 'password']
+        fields = ['email', 'name', 'last_name', 'password', 'is_staff']
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
+        token['name'] = user.name
+        token['last_name'] = user.last_name
         token['email'] = user.email
         token['is_staff'] = user.is_staff
         return token
