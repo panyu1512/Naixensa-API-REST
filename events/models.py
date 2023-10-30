a9da2d983bd967efeecc4584240c046bc824c6f5
panyu1512 <kikeferreragius1997@gmail.com>
2023-10-30 19:07:45 +0100

    update events model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from users.models import User
 
 # Create your models here.
+class Events(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+    date = models.DateField()
+    created_at = models.DateTimeField(default=timezone.now)
+    open_inscription = models.BooleanField(default=True)
+    assistants = models.ManyToManyField(User, related_name='assistants', blank=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.title, self.date)
+    class Meta:
+        ordering = ('-created_at',)
