c0a55d04298822c70e554989bbf7b1cb78dee18c
panyu1512 <kikeferreragius1997@gmail.com>
2023-10-30 14:21:54 +0100

    update build sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
-pip install -r requirements.txt
+set -o errexit
 
+poetry install
+
+python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --no-input
